dd45da9837eb13820dbaebae103b669f5b17b540
jujuuwow <163137149+jujuuwow@users.noreply.github.com>
2024-03-12 09:50:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://veja.abril.com.br/wp-content/uploads/2016/06/justin-bieber-mtv-reino-unido-20111107-01-original3.jpeg?quality=90&strip=info&w=720&h=440&crop=1";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Justin_Bieber.jpg/210px-Justin_Bieber.jpg";
+}
